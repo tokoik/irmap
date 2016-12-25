317e11c55d3bddfee75b2bc44f412c12cb277e13
Kohe Tokoi <tokoik@gmail.com>
2016-12-25 17:42:39 +0900

    add fbo

--- irmap/main.cpp
+++ irmap/main.cpp
@@ -7,6 +7,12 @@
 // 最初に開くウィンドウのサイズ
 const GLsizei width(960), height(540);
 
+// フレームバッファオブジェクトのサイズ
+const GLsizei fboWidth(width), fboHeight(height);
+
+// 境界色
+const GLfloat border[] = { 0.0f, 0.0f, 0.0f, 0.0f };
+
 // 背景色
 const GLfloat background[] = { 0.0f, 0.0f, 0.0f, 0.0f };
 
@@ -18,6 +24,23 @@ const GgSimpleShader::Light light =
   { 1.0f, 1.0f, 1.0f, 0.0f }, // 鏡面光成分
   { 0.0f, 0.0f, 1.0f, 0.0f }  // 位置
 };
+
+// テクスチャの作成
+GLuint createTexture(GLenum internalFormat, GLsizei width, GLsizei height)
+{
+  GLuint t;
+
+  glGenTextures(1, &t);
+  glBindTexture(GL_TEXTURE_2D, t);
+  glTexImage2D(GL_TEXTURE_2D, 0, internalFormat, width, height, 0, GL_BGR, GL_UNSIGNED_BYTE, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+  glTexParameterfv(GL_TEXTURE_2D, GL_TEXTURE_BORDER_COLOR, border);
+
+  return t;
+}
 
 //
 // メイン
@@ -47,11 +70,36 @@ int main()
   // 背景色を設定する
   glClearColor(background[0], background[1], background[2], background[3]);
 
+  // フレームバッファオブジェクトのカラーバッファに用いるテクスチャを作成する
+  const auto color(createTexture(GL_RGBA, fboWidth, fboHeight));
+
+  // フレームバッファオブジェクトのデプスバッファに用いるテクスチャ (デプスマップ) を作成する
+  const auto depth([] { GLuint t; glGenTextures(1, &t); return t; } ());
+  glBindTexture(GL_TEXTURE_2D, depth);
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, fboWidth, fboHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, NULL);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
+
+  // フレームバッファオブジェクトを作成する
+  const auto fbo([] { GLuint f; glGenFramebuffers(1, &f); return f; } ());
+  glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+  // フレームバッファオブジェクトにカラーバッファを組み込む
+  glFramebufferTexture(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, color, 0);
+
+  // フレームバッファオブジェクトにデプスバッファを組み込む
+  glFramebufferTexture(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, depth, 0);
+
   // ウィンドウが開いている間繰り返す
   while (!window.shouldClose())
   {
-    // ビューポートを設定する
-    window.setViewport();
+    // フレームバッファオブジェクトに描画する
+    glBindFramebuffer(GL_FRAMEBUFFER, fbo);
+
+    // フレームバッファオブジェクトに対するビューポートを設定する
+    glViewport(0, 0, fboWidth, fboHeight);
 
     // 隠面消去を行う
     glEnable(GL_DEPTH_TEST);
